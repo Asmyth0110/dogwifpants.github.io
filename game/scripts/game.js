2a3273d8f7a721e83803f47aa09a1831243cafb8
Asmyth0110 <121247994+Asmyth0110@users.noreply.github.com>
2025-05-23 23:05:03 +0100

    Update game.js

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -28,30 +28,28 @@ const game = new Phaser.Game(config);
 let player, cursors, bullets, lastFired = 0;
 
 function preload() {
-  this.load.image('bg1', 'assets/parallax_layer1.png'); // Farthest
-  this.load.image('bg2', 'assets/parallax_layer2.png'); // Mid
-  this.load.image('bg3', 'assets/parallax_layer3.png'); // Foreground
+  this.load.image('bg1', 'assets/parallax_layer1.png');
+  this.load.image('bg2', 'assets/parallax_layer2.png');
+  this.load.image('bg3', 'assets/parallax_layer3.png');
   this.load.image('player', 'assets/teddy.png');
   this.load.image('bullet', 'assets/bullet.png');
 }
 
 
+
 function create() {
-  this.bg1 = this.add.tileSprite(0, 0, 960, 540, 'bg1').setOrigin(0).setScrollFactor(0);
-  this.bg2 = this.add.tileSprite(0, 0, 960, 540, 'bg2').setOrigin(0).setScrollFactor(0);
-  this.bg3 = this.add.tileSprite(0, 0, 960, 540, 'bg3').setOrigin(0).setScrollFactor(0);
+  this.bg1 = this.add.tileSprite(0, 0, 960, 540, 'bg1').setOrigin(0);
+  this.bg2 = this.add.tileSprite(0, 0, 960, 540, 'bg2').setOrigin(0);
+  this.bg3 = this.add.tileSprite(0, 0, 960, 540, 'bg3').setOrigin(0);
 
   player = this.physics.add.sprite(100, 400, 'player');
   player.setCollideWorldBounds(true);
 
-  bullets = this.physics.add.group({
-    defaultKey: 'bullet',
-    maxSize: 10
-  });
+  bullets = this.physics.add.group({ defaultKey: 'bullet', maxSize: 10 });
 
   cursors = this.input.keyboard.createCursorKeys();
-  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
 }
+
 
 
 function update(time) {
