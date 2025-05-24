9fb540e710a0bdc9c2ca134095da69c0ded62f03
Asmyth0110 <121247994+Asmyth0110@users.noreply.github.com>
2025-05-24 21:04:07 +0100

    Update game.js

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -24,7 +24,7 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let player, cursors, bullets, lastFired = 0;
+let player, cursors;
 let bg1, bg2, bg3;
 
 function preload() {
@@ -32,25 +32,32 @@ function preload() {
   this.load.image('bg2', 'assets/parallax_layer2.png');
   this.load.image('bg3', 'assets/parallax_layer3.png');
   this.load.image('player', 'assets/teddy.png');
-  this.load.image('bullet', 'assets/bullet.png');
 }
 
 function create() {
+  // Parallax layers
   bg1 = this.add.tileSprite(0, 0, 960, 540, 'bg1').setOrigin(0);
   bg2 = this.add.tileSprite(0, 0, 960, 540, 'bg2').setOrigin(0);
   bg3 = this.add.tileSprite(0, 0, 960, 540, 'bg3').setOrigin(0);
 
+  // Player
   player = this.physics.add.sprite(100, 400, 'player');
+  player.setCollideWorldBounds(true);
+
+  // Invisible ground
+  const ground = this.physics.add.staticGroup();
+  ground.create(480, 520, null).setSize(960, 40).setVisible(false);
+  this.physics.add.collider(player, ground);
+
+  // Controls
   cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  // Parallax scrolling
   bg1.tilePositionX += 0.2;
   bg2.tilePositionX += 0.5;
   bg3.tilePositionX += 1;
 
-  // Basic jump
   if (cursors.up.isDown && player.body.touching.down) {
     player.setVelocityY(-500);
   }
